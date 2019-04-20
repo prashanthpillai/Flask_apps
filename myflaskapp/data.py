dfc64ac54dc9c70d2ae82b2f12825232697c031c
prashanthpillai <prashanth.mex@gmail.com>
2019-04-21 01:40:27 +0530

    Testing myflaskapp

---
+++ myflaskapp/data.py
@@ -0,0 +1,25 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'title': 'Article 1',
+            'body': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi corporis cum, facilis illum laborum libero maiores quae repellendus reprehenderit repudiandae. Est in neque nostrum praesentium quae quisquam rem reprehenderit, voluptatem',
+            'author': 'Author 1',
+            'create_date': '01-01-2017'
+        },
+        {
+            'id': 2,
+            'title': 'Article 2',
+            'body': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi corporis cum, facilis illum laborum libero maiores quae repellendus reprehenderit repudiandae. Est in neque nostrum praesentium quae quisquam rem reprehenderit, voluptatem',
+            'author': 'Author 2',
+            'create_date': '01-01-2017'
+        },
+        {
+            'id': 3,
+            'title': 'Article 3',
+            'body': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi corporis cum, facilis illum laborum libero maiores quae repellendus reprehenderit repudiandae. Est in neque nostrum praesentium quae quisquam rem reprehenderit, voluptatem',
+            'author': 'Author 3',
+            'create_date': '01-01-2017'
+        }
+    ]
+    return articles
